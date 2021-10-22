3cb7a02aa5f2d974d3353940bae8185129e2258c
OlgaMartazova <olyamarta34@gmail.com>
2021-10-22 18:15:50 +0300

    fragments

--- app/src/main/java/com/itis/secondcourseitis/MainActivity.kt
+++ app/src/main/java/com/itis/secondcourseitis/MainActivity.kt
@@ -2,6 +2,8 @@ package com.itis.secondcourseitis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.ERROR
 import com.itis.secondcourseitis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
+        }
+
+        supportFragmentManager.beginTransaction().run {
+            add(R.id.container, HomeFragment())
+            commit()
+        }
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnHome.setOnClickListener {
+            Log.e("LOG", "btnHome нажата")
+            supportFragmentManager.beginTransaction().run {
+                setCustomAnimations(R.animator.enter_from_right, R.animator.exit_to_left, R.animator.enter_from_left, R.animator.exit_to_right)
+                replace(R.id.container, HomeFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        binding.btnSearch.setOnClickListener {
+            Log.e("LOG", "btnSearch нажата")
+            supportFragmentManager.beginTransaction().run {
+                setCustomAnimations(R.animator.enter_from_right, R.animator.exit_to_left, R.animator.enter_from_left, R.animator.exit_to_right)
+                replace(R.id.container, SearchFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.btnAdd.setOnClickListener {
+            Log.e("LOG", "btnAdd нажата")
+            supportFragmentManager.beginTransaction().run {
+                setCustomAnimations(R.animator.enter_from_right, R.animator.exit_to_left, R.animator.enter_from_left, R.animator.exit_to_right)
+                replace(R.id.container, AddFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.btnInbox.setOnClickListener {
+            Log.e("LOG", "btnInbox нажата")
+            supportFragmentManager.beginTransaction().run {
+                setCustomAnimations(R.animator.enter_from_right, R.animator.exit_to_left, R.animator.enter_from_left, R.animator.exit_to_right)
+                replace(R.id.container, MoveFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.btnMe.setOnClickListener {
+            Log.e("LOG", "btnMe нажата")
+            supportFragmentManager.beginTransaction().run {
+                setCustomAnimations(R.animator.enter_from_right, R.animator.exit_to_left, R.animator.enter_from_left, R.animator.exit_to_right)
+                replace(R.id.container, ProfileFragment())
+                addToBackStack(null)
+                commit()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/itis/secondcourseitis/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.itis.secondcourseitis
+
+import androidx.fragment.app.Fragment
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
