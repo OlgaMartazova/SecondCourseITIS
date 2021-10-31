bffcc9761c7004f778deeb6b7ff52bdc62fc8c42
OlgaMartazova <olyamarta34@gmail.com>
2021-10-31 13:35:05 +0300

    navComponent

--- app/src/main/java/com/itis/secondcourseitis/MainActivity.kt
+++ app/src/main/java/com/itis/secondcourseitis/MainActivity.kt
@@ -2,15 +2,19 @@ package com.itis.secondcourseitis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import com.itis.secondcourseitis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var controller: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
+
+        controller = findController(R.id.container)
     }
 }
