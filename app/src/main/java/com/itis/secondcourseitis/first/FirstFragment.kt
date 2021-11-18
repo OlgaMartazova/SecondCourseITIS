a50f3e38d0b7f1f7bc9aecc49f21c5c996485cf0
OlgaMartazova <olyamarta34@gmail.com>
2021-11-18 14:27:06 +0300

    data added

--- app/src/main/java/com/itis/secondcourseitis/first/FirstFragment.kt
+++ app/src/main/java/com/itis/secondcourseitis/first/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.itis.secondcourseitis.first
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,5 +18,11 @@ class FirstFragment : Fragment() {
     ): View {
         binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textView.text = "Hello! \n \n There are different types of windows and doors here, enjoy and read the description."
     }
 }
